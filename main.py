1f2c7e6d5848c2ec326fc7071154cb433f0bec01
Anders Pihlblad <anderspihlblad1998@gmail.com>
2022-09-12 23:48:41 +0000

    added logo art to art.py file

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Higher Lower Display art
+
+# Generate a random account from the game data.
+
+# Format the account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count of each account.
+## Use if statement to check if user is correct.
+
+# Give feedback on their guess.
+
+# Score keeping. 
+
+# Making the game repeatable.
+
+# Making account at position B become the next account at the position A.
+
+# Clear the screen between rounds.
