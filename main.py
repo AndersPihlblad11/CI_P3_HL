28c0ead6a0fd572660425392bc2743fba49d6760
Anders Pihlblad <anderspihlblad1998@gmail.com>
2022-09-13 00:56:15 +0000

    added code to make account data printable

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from art import logo
 from game_data import data
 import random
 
+# Format the account data into printable format.
+account_name = account_a["name"]
+account_name = account_a["description"]
+account_name = account_a["country"]
+print(f"{account_name}, a {account_descr}, from {account_country}")
+
 # Higher Lower Display art
 print(logo)
 # Generate a random account from the game data.
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+    account_ = random.choice(data)
 
-# Format the account data into printable format.
+print(f"Compare A: {format_data(account_a)}.")
+print(f"Compare B: {format_data(account_a)}.")
 
 # Ask user for a guess.
 
