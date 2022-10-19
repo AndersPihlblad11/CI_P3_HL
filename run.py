5e0de8aefc9dbf25cb4ecc136219b0b23386d959
Anders Pihlblad <anderspihlblad1998@gmail.com>
2022-10-19 04:13:25 +0000

    fixed code bugs regarding layout

--- run.py
+++ run.py
@@ -2,31 +2,35 @@ from art import logo, vs
 from game_data import data
 import random
 
+
 def get_random_account():
-   """
-   Retreive data from random account
-   """
-   return random.choice(data)
+    """
+    Retreive data from random account
+    """
+    return random.choice(data)
+
 
 def format_data(account):
-   """
-   Format account into printable format
-   """
-   name = account ["name"]
-   description = account ["description"]
-   country = account ["country"] 
-   return f"{account_name}, a {account_descr}, from {account_country}"
- 
+    """
+    Format account into printable format
+    """
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"] 
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+
 def check_answer(guess, a_followers, b_followers): 
-   """
-   Checks followers against user's guess 
-   and returns True if they got it right.
-   Or False if they got it wrong.
-   """
-   if a_followers > b_followers:
+    """
+    Checks followers against user's guess 
+    and returns True if they got it right.
+    Or False if they got it wrong.
+    """
+    if a_followers > b_followers:
         return guess == "a"
-   else:
-        return guess == "b" 
+    else:
+        return guess == "b"
+
 
 def game():
     print(logo)
@@ -35,38 +39,36 @@ def game():
     account_a = random.choice(data)
     account_b = random.choice(data)
 
-# Make the game repeatable.
     while game_should_continue:
-# Making account at position B become the next account at the position A.
-       account_a = account_b
-       account_b = random.choice(data)
-
-       while account_a == account_b:
+        """
+        Making account at position B become the next account at the position A.
+        """
+        account_a = account_b
         account_b = random.choice(data)
 
-       print(f"Compare A: {format_data(account_a)}.")
-       print(vs)
-       print(f"Compare B: {format_data(account_a)}.")
+        while account_a == account_b:
+            account_b = random.choice(data)
 
-# Ask user for a guess.
-       input("Who has more followers? Type 'A' or 'B': \n ").lower()
+            print(f"Compare A: {format_data(account_a)}.")
+            print(vs)
+            print(f"Compare B: {format_data(account_a)}.")
 
-# Check if user is correct.
-# Get follower count of each account.
-       a_follower_count = account_a["follower_count"]
-       b_follower_count = account_b["follower_count"]
-       is_correct = check_answer(guess, a_follower_count, b_follower_count)
+            guess = input("Who has more followers? Type 'A' or 'B': \n ").lower()
+            # Check if user is correct.
+            # Get follower count of each account.
+            a_follower_count = account_a["follower_count"]
+            b_follower_count = account_b["follower_count"]
+            is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-   # Clear the screen between rounds.
-       clear()
-       print(logo)
+            clear()
+            print(logo)
+            # Give feedback on their guess.
+            # Score keeping.
+            if is_correct:
+                score += 1
+                print("Your right! Current score: {score}")
+            else:
+                game_should_continue = False
+                print("Sorry, thats wrong. Final score: {score}")
 
-   # Give feedback on their guess.
-   # Score keeping.
-       if is_correct:
-             score += 1
-             print("Your right! Current score: {score}")
-        else:
-        game_should_continue = False
-        print("Sorry, thats wrong. Final score: {score}")
-
+            game()
