46895729b0917e1403c45fc445c9ca4e055d3012
Anders Pihlblad <anderspihlblad1998@gmail.com>
2022-10-19 01:28:23 +0000

    added fixing bugs and code

--- run.py
+++ run.py
@@ -2,24 +2,37 @@ from art import logo, vs
 from game_data import data
 import random
 
+def get_random_account():
+   """
+   Retreive data from random account
+   """
+   return random.choice(data)
+
 def format_data(account):
-    account_name = account ["name"]
-    account_descr = account ["description"]
-    account_country = account ["country"] 
-    return f"{account_name}, a {account_descr}, from {account_country}"
+   """
+   Format account into printable format
+   """
+   name = account ["name"]
+   description = account ["description"]
+   country = account ["country"] 
+   return f"{account_name}, a {account_descr}, from {account_country}"
  
 def check_answer(guess, a_followers, b_followers): 
-    if a_followers > b_followers:
+   """
+   Checks followers against user's guess 
+   and returns True if they got it right.
+   Or False if they got it wrong.
+   """
+   if a_followers > b_followers:
         return guess == "a"
-    else:
+   else:
         return guess == "b" 
 
- # Higher Lower Display art
+def game():
     print(logo)
     score = 0
     game_should_continue = True
-
-# Generate a random account from the game data.
+    account_a = random.choice(data)
     account_b = random.choice(data)
 
 # Make the game repeatable.
@@ -28,8 +41,8 @@ def check_answer(guess, a_followers, b_followers):
        account_a = account_b
        account_b = random.choice(data)
 
-       if account_a == account_b:
-       account_b = random.choice(data)
+       while account_a == account_b:
+        account_b = random.choice(data)
 
        print(f"Compare A: {format_data(account_a)}.")
        print(vs)
@@ -44,16 +57,16 @@ def check_answer(guess, a_followers, b_followers):
        b_follower_count = account_b["follower_count"]
        is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-      # Clear the screen between rounds.
+   # Clear the screen between rounds.
        clear()
        print(logo)
 
-       # Give feedback on their guess.
-      # Score keeping.
+   # Give feedback on their guess.
+   # Score keeping.
        if is_correct:
-         score += 1
-       print("Your right! Current score: {score}")
-       else:
-       game_should_continue = False
-       print("Sorry, thats wrong. Final score: {score}")
+             score += 1
+             print("Your right! Current score: {score}")
+        else:
+        game_should_continue = False
+        print("Sorry, thats wrong. Final score: {score}")
 
